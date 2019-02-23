e3f423b57046f335b8c873d91fc01d5ae2591317
dedbyurer <loveytandme@gmail.com>
2019-02-23 03:18:21 +0300

    First commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const botconfig = require("./botconfig.json");
+const Discord = require("discord.js");
+
+const bot = new Discord.Client();
+
+
+
+bot.on("ready", async () => {
+  console.log('${bot.user.username} is online!');
+});
+
+
+
+bot.on("message", async message => {
+  if(message.author.bot) return;
+
+  let prefix = botconfig.prefix;
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+
+
+
+});
+
+bot.login(botconfig.token);
