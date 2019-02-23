80b19b241899b15d8fcb5b68e5080b5f679d31a5
dedbyurer <loveytandme@gmail.com>
2019-02-23 05:07:51 +0300

    а

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ bot.on("message", async message => {
     if(!deleteCount || deleteCount < 2 || deleteCount > 100)
       return message.reply("Укажи, сколько сообщений надо удалить (от 2 до 100)");
 
-    const fetched = await message.channel.fetchMessages({limit: deleteCount}) + 1;
+    const fetched = await message.channel.fetchMessages({limit: deleteCount});
     message.channel.bulkDelete(fetched)
       .catch(error => message.reply(`Не могу удалить сообщения по причине:${error}`));
 
