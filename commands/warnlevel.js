d52f5f16738110823912a9884c00f60c3776a7ef
dedbyurer <loveytandme@gmail.com>
2019-02-25 20:52:23 +0300

    f

--- commands/warnlevel.js
+++ commands/warnlevel.js
@@ -2,24 +2,30 @@ const Discord = require("discord.js");
 const fs = require("fs");
 const ms = require("ms");
 let warns = JSON.parse(fs.readFileSync("./warnings.json", "utf8"));
+var Warn = require('./../models/warn.js');
 
 const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;mongoose.connect(process.env.DATA_LOGINS, { useNewUrlParser: true });
 
 module.exports.run = async (bot, message, args) => {
 let wUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
-
-  if(!warns[wUser.id]) warns[wUser.id] = {
-    warns: 0
-  };
-
-
-
   if(!wUser) return message.reply("Нет такого челика :(");
-  let warnlevel = warns[wUser.id].warns;
 
-  message.reply(`<@${wUser.id}> получил ${warnlevel} предупреждений.`);
+  var user_obj = Warn.findOne({
+      userID: wUser.id
+    }, async function (err, foundObj) {
+      if (err)
+        console.log("Error on database findOne: " + err);
+      else {
+        if (!foundObj)
+          console.log("Something stange happend");
+        else {
+          var numberWarnings = user_obj.numberWarnings;
+          message.reply(`<@${wUser.id}> получил ${numberWarnings} предупреждений.`);
 
+       }
+      }
+    }
 }
 
 module.exports.help = {
