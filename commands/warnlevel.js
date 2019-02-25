6b7f59a35e2c8df4830745ad981449c7f543c0d1
dedbyurer <loveytandme@gmail.com>
2019-02-25 20:09:21 +0300

    а

--- commands/warnlevel.js
+++ commands/warnlevel.js
@@ -7,7 +7,7 @@ const mongoose = require("mongoose");
 mongoose.Promise = Promise
 
 module.exports.run = async (bot, message, args) => {
-mongoose.connect('mongodb+srv://raidenshow:Sosipisos228@rraidenterritory-nmzid.mongodb.net/Warns',  { useNewUrlParser: true });
+mongoose.connect(DATA_LOGINS,  { useNewUrlParser: true });
 let wUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
 
   if(!warns[wUser.id]) warns[wUser.id] = {
