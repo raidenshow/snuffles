24c25b035e380393b7465887b8e4e51f99387094
dedbyurer <loveytandme@gmail.com>
2019-02-25 13:48:30 +0300

    h

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
 const fs = require("fs");
+const bot = new Discord.Client();
 bot.commands = new Discord.Collection();
 
 
@@ -24,8 +25,6 @@ fs.readdir("./commands/", (err, files) => {
 
 
 });
-
-const bot = new Discord.Client();
 
 const modRoles = botconfig.modRoles;
 const logChannel= botconfig.logChannel;
