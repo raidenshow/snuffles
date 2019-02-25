009b51ccff6ea0229a01420b05843de3c5c338f4
dedbyurer <loveytandme@gmail.com>
2019-02-25 21:06:51 +0300

    g

--- commands/warnlevel.js
+++ commands/warnlevel.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const fs = require("fs");
 const ms = require("ms");
 let warns = JSON.parse(fs.readFileSync("./warnings.json", "utf8"));
-var Warn = require('./models/warn.js');
+var Warn = require('./../models/warn.js');
 
 const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;mongoose.connect(process.env.DATA_LOGINS, { useNewUrlParser: true });
--- models/warn.js
+++ models/warn.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const warnSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
-  userID: String,
+  userID: Number,
   userNickname: String,
   warnReason: String,
   moderatorID: String,
