fe0d75ec3cf2807d908a00538f768453dc2fef9c
dedbyurer <loveytandme@gmail.com>
2019-02-26 00:47:12 +0300

    f

--- commands/scanuser.js
+++ commands/scanuser.js
@@ -3,6 +3,7 @@ const bot = new Discord.Client();
 const botconfig = require("./botconfig.json");
 const modRoles = botconfig.modRoles;
 const logChannel= botconfig.logChannel;
+const mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.DATA_LOGINS);
