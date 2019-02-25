ee23520dc1f78be0b7f75b6d32ffe15e588211cc
dedbyurer <loveytandme@gmail.com>
2019-02-25 19:36:08 +0300

    f

--- commands/warnlevel.js
+++ commands/warnlevel.js
@@ -4,12 +4,12 @@ const ms = require("ms");
 let warns = JSON.parse(fs.readFileSync("./warnings.json", "utf8"));
 
 module.exports.run = async (bot, message, args) => {
-
+let wUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
   if(!warns[wUser.id]) warns[wUser.id] = {
     warns: 0
   };
 
-  
+
   let wUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
   if(!wUser) return message.reply("Нет такого челика :(");
   let warnlevel = warns[wUser.id].warns;
